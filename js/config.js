eaa9b7b840ec53b45111919acff9939adb2c79c7
Mir <mir.m.ali01@gmail.com>
2023-06-26 19:32:03 -0400

    copied static files from S3 bucket

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_CH8i3v88H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '737bsmu6uf12ufjcjmd3j52235', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://m7gmywdcqk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
